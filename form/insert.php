311cf5798f147d2466f54ecd8ae293afcb20fa75
SathitChitbamrung01 <matsumotoryuuji2@gmail.com>
2022-08-15 04:49:14 +0100

    first commit

---
+++ form/insert.php
@@ -0,0 +1,33 @@
+<?php
+ $name = $_POST['txt_firstname'];
+echo $name;
+echo "<br>";
+
+$txt_name = $_POST['txt_name'];
+echo $txt_name;
+echo "<br>";
+
+$firstname = $_POST['firstname'];
+echo $firstname;
+echo "<br>";
+
+$sex = $_POST['sex'];
+echo $sex;
+echo "<br>";
+
+$txt_birthdate = $_POST['txt_birthdate'];
+echo $txt_birthdate;
+echo "<br>";
+
+$text_address = $_POST['text_address'];
+echo $text_address;
+echo "<br>";
+
+$telephone_number = $_POST['telephone_number'];
+echo $telephone_number;
+echo "<br>";
+
+$email = $_POST['email'];
+echo $email;
+echo "<br>";
+?>
